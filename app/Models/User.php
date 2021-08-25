9c84bdc555b9218a7aaf0f9d9e745bf07d87975c
vagrant <vagrant@homestead>
2021-08-25 15:09:39 +0000

    Add new methods to the User model which we will use to get roles attach and update roles.

--- app/Models/User.php
+++ app/Models/User.php
@@ -34,4 +34,37 @@ class User extends Model
      * @var array
      */
     protected $casts = [];
+
+	/**
+	 * The roles that belong to the user.
+	 */
+	public function roles()
+	{
+		return $this->belongsToMany('App\Models\Role');
+	}
+
+	/**
+	 * Attach roles to user.
+	 *
+	 * @param $roleIds
+	 */
+	public function assignRoles($roleIds)
+	{
+		foreach ($roleIds as $roleId)
+		{
+			$role = Role::findOrFail($roleId);
+			$this->roles()->attach($role);
+		}
+	}
+
+	/**
+	 * Update the roles of the user.
+	 *
+	 * @param $roleIds
+	 */
+	public function updateRoles($roleIds)
+	{
+		$this->roles()->sync([]);
+		$this->assignRoles($roleIds);
+	}
 }
